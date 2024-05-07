9df552916227759430eae526cbe2f573fe6237c0
srakhya542 <117500558+srakhya542@users.noreply.github.com>
2024-05-07 19:45:40 +0530

    Add files via upload

---
+++ routes/postRoutes.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+
+ const {createPost,getPosts,getPost,getCatPosts,getUserPosts,editPost,deletePost,removeEventListener} = require('../controllers/postControllers')
+
+const router = Router()
+
+const authMiddleware=require('../middleware/authMiddleware')
+
+router.post('/',authMiddleware,createPost)
+router.get('/',getPosts)
+router.get('/:id',getPost)
+router.get('/categories/:category',getCatPosts)
+router.get('/users/:id',getUserPosts)
+router.patch('/:id',authMiddleware,editPost)
+router.delete('/:id',authMiddleware,deletePost)
+
+
+
+module.exports=router
